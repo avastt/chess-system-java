2dbf56b61d64fb0372c96cc9c45fc248eff33474
igor.frey <igor.frey@dataprev.gov.br>
2019-03-12 15:41:43 -0300

    Testing

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -26,14 +26,14 @@ public class Board {
 	public Piece piece(int row, int column) {
 
 		if (!positionExists(row, column)) {
-			throw new BoardException("Position not on the board");
+			throw new BoardException("Position not on the board!");
 		}
 		return pieces[row][column];
 	}
 
 	public Piece piece(Position position) {
 		if (!positionExists(position)) {
-			throw new BoardException("Position not on the board");
+			throw new BoardException("Position not on the board!");
 		}
 		return pieces[position.getRow()][position.getColumn()];
 	}
@@ -56,7 +56,7 @@ public class Board {
 
 	public boolean thereIsAPiece(Position position) {
 		if (!positionExists(position)) {
-			throw new BoardException("Position not on the board");
+			throw new BoardException("Position not on the board!");
 		}
 		return piece(position) != null;
 	}
